13098c0690cb9ff6fc1574a530d2a03f552e4d96
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-07-14 16:58:06 -0300

    Criando os components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
 
 function App() {
   return (
     <div className="App">
-      <p>Costs</p>
+      <Router>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
