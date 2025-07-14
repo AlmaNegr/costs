d1694a61a6d0630a867f790692328ad597994c9f
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-07-14 17:42:23 -0300

    projeto-cost browserrouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Company from "./components/pages/Company";
 import NewProject from "./components/pages/NewProject";
+import NavBar from "./components/pages/NavBar";
+import Container from "./components/layout/Container";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Container>
         <Routes>
-          <Route></Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/company" element={<Company />}></Route>
+            <Route path="/newproject" element={<NewProject />} ></Route>
         </Routes>
-      </Router>
-    </div>
+        </Container>
+      </div>
+      <p>Footer</p>
+    </Router>
   );
 }
 
