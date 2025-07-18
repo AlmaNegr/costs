40283101f8fd4ece282705c70ba8235e292fd279
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-07-18 06:37:48 -0300

    Componentização de formulário

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,13 +1,24 @@
+import styles from './ProjectForm.module.css'
+import Input from '../form/Input'
+import Select from '../form/Select'
+
 function ProjectForm() {
     return(
-        <form>
-            <div><input type="text" placeholder="Insira o nome do projeto"></input></div>
-            <div><input type="number" placeholder="Insira o orçamento total"></input></div>
-            <div>
-                <select id="categoty_id">
-                    <option disabled>Selecione a categoria</option>
-                </select>
-            </div>
+        <form className={styles.form}>
+            <Input 
+                type="text"
+                text="Nome do projeto"
+                name="name"
+                placeholder="Insira o nome do seu projeto"
+            />
+            <Input 
+                type="number"
+                text="Orçamento do projeto"
+                name="budget"
+                placeholder="Insira o orçamento total"
+            />
+            <Select name="category_id" text="Selecione a categoria"/>
+            
             <div>
                 <input type="submit" value="Criar projeto"></input>
             </div>
