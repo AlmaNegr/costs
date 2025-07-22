f0fbb67bf06864dd4ad49843f5437ba5cbc6d2fd
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-07-22 16:49:03 -0300

    projeto costs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Container from './components/layout/Container'
+import Home from './components/pages/Home'
+import Projects from './components/pages/Projects'
+import NewProject from './components/pages/NewProject'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import Project from './components/pages/Project'
 
 function App() {
-  return(
+  return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/company" element={<Company />}></Route>
-        <Route path="/contato" element={<Contact />}></Route>
-        <Route path="newproject" element={<NewProject />}></Route>
-      </Routes>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+          <Route path="/project/:id" element={<Project />} />
+        </Routes>
+      </Container>
+      <Footer />
     </Router>
   )
 }
